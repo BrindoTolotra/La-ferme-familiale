c122f26fee2c0f1daef57fe4f4276bcaad73645f
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-03-11 04:28:53 +0100

    Update code

--- Solution/La ferme familiale.js
+++ Solution/La ferme familiale.js
@@ -4,8 +4,9 @@
 */
 let lettre = (chaine1,chaine2) =>{
     let reponse = "";
+    // i pour parcourir la chaine 1 et j pour la chaine 2
     for(let i = 0; i<chaine1.length;i++){
-        for(let j = i; j<i+1;j++){
+        for(let j = i; j<i+1; j++){
             reponse = reponse + chaine1[i]+chaine2[j];
         }
     }
